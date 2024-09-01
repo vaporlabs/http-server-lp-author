21352e64a2781431285339e3a00a2f71bdd8dc36
vapr <121268755+vaporlabs@users.noreply.github.com>
2024-09-01 12:06:53 -0700

    Add /parrot endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -23,6 +23,15 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
             Ok(Response::new(Body::from(reversed_body)))
         }
 
+        (&Method::POST, "/parrot") => {
+            let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
+            let body_string = String::from_utf8(body_bytes.to_vec()).unwrap();
+            let mut reply = String::from("You said: ");
+            reply.push_str(&body_string);
+            Ok(Response::new(Body::from(reply)))
+
+        }
+
         // Return the 404 Not Found for other routes.
         _ => {
             let mut not_found = Response::default();
